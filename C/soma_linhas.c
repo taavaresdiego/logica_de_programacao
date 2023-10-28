2efd862ea91b5072bcb9815a3ec0d61faf804a87
Diego Tavares <mello.gredy@gmail.com>
2023-10-28 14:16:02 -0300

    Finalizando a lista de exercícios em C

---
+++ C/soma_linhas.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+
+  int M, N;
+
+  printf("Qual a quantidade de linhas da matriz? ");
+  scanf("%d", &M);
+  printf("Qual a quantidade de colunas da matriz? ");
+  scanf("%d", &N);
+
+  double matriz[M][N];
+  double vet[M];
+
+  for (int i = 0; i < M; i++)
+  {
+    printf("Digite os elementos da %da. linha:\n ", i + 1);
+    for (int j = 0; j < N; j++)
+    {
+      scanf("%lf", &matriz[i][j]);
+    }
+  }
+
+  for (int i = 0; i < M; i++)
+  {
+    vet[i] = 0;
+    for (int j = 0; j < N; j++)
+    {
+      vet[i] += matriz[i][j];
+    }
+  }
+
+  printf("VETOR GERADO:\n");
+  for (int i = 0; i < M; i++)
+  {
+    printf("%.1lf\n", vet[i]);
+  }
+
+  return 0;
+}
