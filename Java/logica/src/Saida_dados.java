92e4daffb73ea662e66b5f79fd744b0a499c820c
Diego Tavares <mello.gredy@gmail.com>
2023-11-06 23:21:32 -0300

    Inicializando exercícios básicos em Java

---
+++ Java/logica/src/Saida_dados.java
@@ -0,0 +1,21 @@
+public class Saida_dados {
+    public static void main(String[] args) throws Exception {
+        int idade;
+        double salario, altura;
+        char genero;
+        String nome;
+
+        idade = 30;
+        salario = 5000.5;
+        altura = 1.73;
+        genero = 'F';
+        nome = "Carolina";
+
+        System.out.println("Idade = " + idade); // saida de dados em java 
+        System.out.println("Salario = " + salario);
+        System.out.println("Altura = " + altura);
+        System.out.println("Genero = " + genero);
+        System.out.println("Nome = " + nome);
+
+    }
+}
